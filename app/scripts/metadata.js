5603324759323cdb17441a9bfa1e87217e6b1f43
Sas Sam <496429+SasSam@users.noreply.github.com>
2025-09-13 19:30:54 +0100

    Updated metadata

--- app/scripts/metadata.js
+++ app/scripts/metadata.js
@@ -1,4 +1,4 @@
 if(typeof window.tfpc === "undefined") {
 	window.tfpc = {};
 }
-window.tfpc.metadata = {"version":"1.4.2","revision":{"label":"ae6e880-dirty","hash":"ae6e880"}};
+window.tfpc.metadata = {"version":"1.4.2","revision":{"label":"6fba294","hash":"6fba294"}};
